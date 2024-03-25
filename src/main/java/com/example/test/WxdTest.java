6510b4595b08fb07cbedf00149be1786f414c320
祥栋 <wangxiangdong@hvyogo.com>
2024-03-25 10:48:15 +0800

    fix

---
+++ src/main/java/com/example/test/WxdTest.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import lombok.Data;
+
+/**
+ * \* Created with WXD.
+ * \* Date:  2024/3/25
+ * \* Description:
+ * \* @author 王祥栋
+ */
+@Data
+public class WxdTest {
+    public static void main(String[] args) {
+
+    }
+}
